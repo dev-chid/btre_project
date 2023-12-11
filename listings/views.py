eb20f3ba99a6f1b80fbffcfe8eb66b64e14d5c63
Chidambararaja B <chidb@cisco.com>
2023-12-11 16:17:30 +0530

    page value

--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,7 @@ from .models import Listing
 
 def index(request):
     listings = Listing.objects.all()
-    paginator = Paginator(listings, 1)
+    paginator = Paginator(listings, 2)
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)

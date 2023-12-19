8087b38ad9be9d23bbcb8ccaf6e55ec11b2a6492
Chidambararaja B <chidb@cisco.com>
2023-12-20 01:02:29 +0530

    Search Feature

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from listings.models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
     listings = Listing.objects.all()[:1]
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
     return render(request, 'pages/index.html', context)
 

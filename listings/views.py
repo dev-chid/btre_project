d29727e84cbf1762b88f42f0b653eb3f6c119e46
Chidambararaja B <chidb@cisco.com>
2023-12-19 18:08:28 +0530

    listing

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, Page
+from .models import Listing
 
-def index(requet):
-    return render(requet, 'listings/listings.html')
+def index(request):
+    listings = Listing.objects.all()
+    paginator = Paginator(listings, 2)
 
-def listing(requet):
-    return render(requet, 'listings/listing.html')
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
-def search(requet):
-    return render(requet, 'listings/search.html')
+    context = {
+        'listings': page_obj
+    }
+
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
+
+def search(request):
+    return render(request, 'listings/search.html')

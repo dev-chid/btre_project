2494f0cbe18182d17dc73f2fcd86376c47c6e3b8
Chidambararaja B <chidb@cisco.com>
2023-12-20 00:11:21 +0530

    Listings added in Index

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from listings.models import Listing
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.all()[:1]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')

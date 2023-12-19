1e7266eed148f14cf9a9eda3849212586c867fa1
Chidambararaja B <chidb@cisco.com>
2023-12-19 17:30:38 +0530

    Template Index and Partials added

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'pages/index.html')
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
